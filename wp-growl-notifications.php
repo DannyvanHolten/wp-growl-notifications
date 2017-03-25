d6a3b805898788ce82e86c84eff79eaf9abb280e
Danny van Holten <info@dannyvanholten.com>
2017-03-25 21:44:56 +0100

    Name changing

--- wp-growl-notifications.php
+++ wp-growl-notifications.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP Growl Notifications
-Plugin URI: https://github.com/DannyvanHolten/wp-growl
+Plugin URI: https://github.com/DannyvanHolten/wp-growl-notifications
 Description: Show your WordPress admin notices in a growlike / better way, and snooze them whenever you see fit.
 Version: 1.0
 Author: Danny van Holten
