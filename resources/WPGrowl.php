a23261ed2e831242dcc8e219c43c6cb4098fee4e
Danny van Holten <info@dannyvanholten.com>
2017-03-27 18:34:32 +0200

    Multilingual

--- resources/WPGrowl.php
+++ resources/WPGrowl.php
@@ -16,11 +16,30 @@ class WPGrowl
 
 	private function init()
 	{
-		add_action('admin_enqueue_scripts', [$this, 'loadStyles']);
+		add_action('admin_enqueue_scripts', [$this, 'loadScripts']);
 		add_action('wp_ajax_wp-growl-snooze-notice', [$this, 'snoozeNotice']);
+		add_action('admin_init', [$this, 'loadTextDomain']);
 	}
 
-	public function loadStyles()
+	/**
+	 * Shoot our translation strings into javascript
+	 *
+	 * @return array
+	 */
+	private function growlTranslations()
+	{
+		return [
+			'hour' => __('Remind me in an Hour', 'wp-growl-notifications'),
+			'day'  => __('Remind me tomorrow', 'wp-growl-notifications'),
+			'week' => __('Remind me next week', 'wp-growl-notifications'),
+			'year' => __('Ignore this message', 'wp-growl-notifications')
+		];
+	}
+
+	/**
+	 * Load all the scripts necessary for our plugin
+	 */
+	public function loadScripts()
 	{
 		wp_enqueue_script('jquery-initialize', WP_GROWL_ASSETS . 'js/jquery-initialize.min.js', ['jquery'],
 			WP_GROWL_VERSION, true);
@@ -36,21 +55,39 @@ class WPGrowl
 		wp_enqueue_script('growl-js');
 	}
 
-	public function snoozedNotices()
+	/**
+	 * Load the gettext plugin textdomain located in our language directory.
+	 */
+	public function loadTextDomain()
 	{
+		load_plugin_textdomain('wp-growl-notifications', false, WP_GROWL_ASSETS . '/language');
+	}
+
+	/**
+	 * Get our snoozed notices from the database and send them to our javascript
+	 *
+	 * @return mixed
+	 */
+	private function snoozedNotices()
+	{
+		// Get our notices from the database
 		$notices = get_user_meta(get_current_user_id(), 'wp-growl-snoozed-notices', true);
 		$oldNotices = $notices;
 
 		if (!empty($notices)) {
 			foreach ($notices as $notice => $wakeup) {
+				// Check if we still need to snooze them
 				if ($wakeup < time()) {
+					// Delete them if not
 					unset($notices[$notice]);
 				} else {
+					// Strip slashed from them if so
 					unset($notices[$notice]);
 					$notices[stripslashes($notice)] = $wakeup;
 				}
 			}
 
+			// Remove expired snoozes if they're not the same as before
 			if ($oldNotices !== $notices) {
 				update_user_meta(get_current_user_id(), 'wp-growl-snoozed-notices', $notices);
 			}
@@ -59,9 +96,12 @@ class WPGrowl
 		return $notices;
 	}
 
+	/**
+	 * The Ajax call to snooze our notices
+	 */
 	public function snoozeNotice()
 	{
-
+		// Check if we've got all the data
 		if (empty($_POST['notice'] || empty($_POST['wakeup']))) {
 			die(
 			json_encode(
@@ -73,12 +113,12 @@ class WPGrowl
 			);
 		}
 
+		// Add it to the list!
 		$notices = get_user_meta(get_current_user_id(), 'wp-growl-snoozed-notices', true);
-
 		$notices[$_POST['notice']] = $_POST['wakeup'];
-
 		update_user_meta(get_current_user_id(), 'wp-growl-snoozed-notices', $notices);
 
+		// Return succes
 		die(
 		json_encode(
 			[
@@ -86,14 +126,5 @@ class WPGrowl
 			]
 		)
 		);
-	}
-
-	public function growlTranslations() {
-		return [
-			'hour' => __('Remind me in 1 Hour', 'wp-growl'),
-			'day' => __('Remind me tomorrow', 'wp-growl'),
-			'week' => __('Remind me in 1 week', 'wp-growl'),
-			'year' => __('Ignore this notification', 'wp-growl')
-		];
 	}
 }
